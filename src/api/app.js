4f5aa25b9d764b5b177e6dc29a1e4570d1a17f50
doCodingWhileCoding <nadirboughlalajob@gmail.com>
2023-09-20 02:36:35 +0200

    added helmet security

--- src/api/app.js
+++ src/api/app.js
@@ -2,11 +2,13 @@ import express from 'express'
 import config from '../config/index.js'
 import cors from 'cors'
 import { corsConfig } from './libs/index.js'
+import helmet from 'helmet'
 
 const app = express()
 
 app.set('port', config.NODE_INTERNAL_PORT)
 
 app.use(cors(corsConfig))
+app.use(helmet())
 
 export default app
