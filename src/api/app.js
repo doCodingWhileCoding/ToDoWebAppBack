d395da71aa925353e5e4b36f62af2b0f1bcf4750
doCodingWhileCoding <nadirboughlalajob@gmail.com>
2023-09-20 02:44:42 +0200

    added morgan to get info about petitions

--- src/api/app.js
+++ src/api/app.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import { corsConfig } from './libs/index.js'
 import helmet from 'helmet'
 import { globalLimiter } from './libs/index.js'
+import morgan from 'morgan'
 
 const app = express()
 
@@ -12,5 +13,9 @@ app.set('port', config.NODE_INTERNAL_PORT)
 app.use(cors(corsConfig))
 app.use(helmet())
 app.use(globalLimiter)
+
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('dev')) //Returns information in console about petitions (GET,PUT,POST,DELETE)
+}
 
 export default app
