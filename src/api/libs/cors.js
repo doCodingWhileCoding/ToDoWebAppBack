be89b353b2703e41268a404b3cd174c1fa4cba2b
doCodingWhileCoding <nadirboughlalajob@gmail.com>
2024-02-15 18:49:59 +0100

    updated the cors config

--- src/api/libs/cors.js
+++ src/api/libs/cors.js
@@ -1,5 +1,7 @@
 import config from '../../config/dotenv.js'
 const corsConfig = {
   origin: config.FRONT_BASE_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }
 export default corsConfig
